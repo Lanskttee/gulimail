14e6512e9110359f9f982a09c18ea02b1e9e4607
yu ge <yuge375841@ruc.edu.cn>
2024-05-06 14:01:01 +0900

    项目基本骨架创建

--- gulimail-product/src/main/java/com/atguigu/gulimall/product/GulimailProductApplication.java
+++ gulimail-product/src/main/java/com/atguigu/gulimall/product/GulimailProductApplication.java
@@ -1,9 +1,11 @@
-package com.atguigu.com.product;
+package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.atguigu.gulimall.product.dao")
 public class GulimailProductApplication {
 
     public static void main(String[] args) {
---
+++ gulimail-product/src/test/java/com/atguigu/gulimall/product/GulimailProductApplicationTests.java
@@ -0,0 +1,27 @@
+package com.atguigu.gulimall.product;
+
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+//import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+
+@SpringBootTest(classes= GulimailProductApplicationTests.class)
+public class GulimailProductApplicationTests {
+//    @Autowired
+//    BrandService brandService;
+
+    @Test
+    public void contextLoads() {
+//        BrandEntity brandEntity=new BrandEntity();
+//
+//        brandEntity.setName("huawei");
+//        brandService.save(brandEntity);
+//        System.out.println("saved");
+    }
+
+}
